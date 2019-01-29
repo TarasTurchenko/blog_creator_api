244ba6cb7efaf710a175b86fcc80ad1aa518521a
taras.turchenko <tarasturhchenko1551@gmail.com>
2019-01-29 18:14:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Blog creator API
+## Environment variables
+```dotenv
+RAILS_ENV
+
+# PostgreSQL
+DB_HOST
+DB_PORT
+DB_DATABASE
+DB_USERNAME
+DB_PASSWORD
+```
---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require_relative 'boot'
+
+require "rails"
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+
+Bundler.require(*Rails.groups)
+
+module BlogCreatorApi
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.generators.system_tests = nil
+  end
+end
