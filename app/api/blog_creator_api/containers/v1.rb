107f21e1b0c7aa1d6120781a92041f22c6768425
TarasTurchenko <tarasturhchenko1551@gmail.com>
2019-01-31 00:07:46 +0200

    added updating container settings

--- app/api/blog_creator_api/containers/v1.rb
+++ app/api/blog_creator_api/containers/v1.rb
@@ -7,16 +7,34 @@ module BlogCreatorApi
 
       params do
         requires :post_id, type: Integer
+        requires :data, type: Hash do
+          requires :position, type: Integer
+        end
       end
-      resources 'posts/:post_id/containers' do
+      post 'posts/:post_id/containers' do
+        position = params[:data][:position]
+        Container.create! post_id: params[:post_id], position: position
+      end
+
+      params do
+        requires :container_id, type: Integer
+      end
+      resources 'containers/:container_id' do
         params do
           requires :data, type: Hash do
-            requires :position, type: Integer
+            optional :offset_top, type: String
+            optional :offset_right, type: String
+            optional :offset_bottom, type: String
+            optional :offset_left, type: String
+            optional :bg_color, type: String
+            optional :bg_image, type: String, url?: true
           end
         end
-        post do
-          position = params[:data][:position]
-          Container.create! post_id: params[:post_id], position: position
+        put do
+          data = declared_params[:data]
+          container = Container.find params[:container_id]
+          container.update! data
+          container
         end
       end
     end
