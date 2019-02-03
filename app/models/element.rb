fc1c4f8e7a8ee9c05cceb7379aaa79320bb157aa
TarasTurchenko <tarasturhchenko1551@gmail.com>
2019-02-03 15:01:37 +0200

    one model for elements

--- app/models/element.rb
+++ app/models/element.rb
@@ -11,14 +11,35 @@
 # integer :position
 # string :kind
 # jsonb :main_settings
-class BaseElement < ApplicationRecord
+class Element < ApplicationRecord
   self.table_name = :elements
 
   include SharedModels::PositionableModel
 
   MAX_SIZE = 12
 
+  DEFAULT_SETTINGS = {
+    text: { content: 'Hey! Your text will be here' }.freeze,
+    image: {
+      src: Constants::Images::PLACEHOLDER,
+      alt: 'Placeholder image'
+    }.freeze,
+    link: {
+      destination_type: :external,
+      destination: 'http://example-link.com',
+      text: 'Example link'
+    }.freeze
+  }.freeze
+
   belongs_to :container
+
+  defaults(
+    offset_top: '20px',
+    offset_right: '5%',
+    offset_bottom: '20px',
+    offset_left: '5%',
+    main_settings: {},
+  )
 
   enum kind: %i(text image link)
 
