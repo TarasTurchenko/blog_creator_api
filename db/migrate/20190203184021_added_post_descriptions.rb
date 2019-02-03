7a43795de3ab30e9e62cd887583ea8cd869d77b3
TarasTurchenko <tarasturhchenko1551@gmail.com>
2019-02-03 22:02:29 +0200

    added description

---
+++ db/migrate/20190203184021_added_post_descriptions.rb
@@ -0,0 +1,9 @@
+class AddedPostDescriptions < ActiveRecord::Migration[5.2]
+  def up
+    add_column :posts, :description, :string
+  end
+
+  def down
+    remove_column :posts, :description
+  end
+end
