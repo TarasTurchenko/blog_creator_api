6ea60d9246abca9f781faa3e625a47d23dc37daf
TarasTurchenko <tarasturhchenko1551@gmail.com>
2019-04-23 21:34:47 +0300

    fix deploy

--- ansible/up_project.sh
+++ ansible/up_project.sh
@@ -5,6 +5,7 @@ cd /home/ubuntu/blog_creator_api/current
 docker-compose -f docker-compose.production.yml stop
 docker-compose -f docker-compose.production.yml build
 docker-compose -f docker-compose.production.yml run --rm web rails db:migrate
+docker-compose -f docker-compose.production.yml run --rm web rails assets:precompile
 docker-compose -f docker-compose.production.yml stop
 docker-compose -f docker-compose.production.yml up -d
 sleep 5
