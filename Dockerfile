f41f9d1198784dc75cc807f684388ee7b3ca9395
TarasTurchenko <tarasturhchenko1551@gmail.com>
2019-05-06 23:30:13 +0300

    add normalize

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ WORKDIR /myapp
 
 RUN apt-get update -y && apt-get install -y openjdk-8-jre
 
+RUN curl -sL https://deb.nodesource.com/setup_10.x | bash
+RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
+RUN npm i -g yarn
+
 COPY Gemfile /myapp/Gemfile
 COPY Gemfile.lock /myapp/Gemfile.lock
 RUN bundle install
--- ansible/up_project.sh
+++ ansible/up_project.sh
@@ -12,6 +12,7 @@ cd /home/ubuntu/blog_creator_api/current
 docker-compose -f docker-compose.production.yml stop
 docker-compose -f docker-compose.production.yml build
 docker-compose -f docker-compose.production.yml run --rm web rails db:migrate
+docker-compose -f docker-compose.production.yml run --rm web yarn install
 docker-compose -f docker-compose.production.yml run --rm web rails assets:precompile
 docker-compose -f docker-compose.production.yml run --rm web rails assets:sync
 docker-compose -f docker-compose.production.yml stop
