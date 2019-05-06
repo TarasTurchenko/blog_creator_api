3e8924b236f09464ccc25f3c1ae2dcb09ef3b073
TarasTurchenko <tarasturhchenko1551@gmail.com>
2019-05-07 00:02:57 +0300

    fix up project script

--- ansible/up_project.sh
+++ ansible/up_project.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-# enable swap
-sudo dd if=/dev/zero of=/swapfile1 bs=1024 count=524288
-sudo chown root:root /swapfile1
-sudo chmod 0600 /swapfile1
-sudo mkswap /swapfile1
-sudo swapon /swapfile1
-
 cd /home/ubuntu/blog_creator_api/current
 
 docker-compose -f docker-compose.production.yml stop
@@ -17,8 +10,5 @@ docker-compose -f docker-compose.production.yml run --rm web rails assets:precom
 docker-compose -f docker-compose.production.yml run --rm web rails assets:sync
 docker-compose -f docker-compose.production.yml stop
 docker-compose -f docker-compose.production.yml up -d
-
-sudo swapoff /swapfile1
-sudo rm /swapfile1
 
 docker ps
